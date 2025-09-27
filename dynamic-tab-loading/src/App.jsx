c3a01a2a66237444fbf8e4d9bb98f8a14b2fd19d
josh098741 <morongemokaya@gmail.com>
2025-09-27 14:56:56 +0300

    Additional commits

---
+++ dynamic-tab-loading/src/App.jsx
@@ -0,0 +1,9 @@
+import TabTest from "./custom-tabs/tab-test";
+
+function App() {
+  return(
+    <TabTest />
+  );
+}
+
+export default App
---
+++ dynamic-tab-loading/src/custom-tabs/tabs.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import './tabs.css'
+
+
+function Tabs({ tabsContent, onChange }) {
+ 
+    const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+    function handleOnClick(getCurrentIndex){
+        setCurrentTabIndex(getCurrentIndex)
+        onChange(getCurrentIndex)
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="heading">
+                {
+                    tabsContent.map((tabItem,index) => (
+                        <div className={`tab-item ${currentTabIndex === index ? 'active':''}`} key={tabItem.label} onClick={() => handleOnClick(index)}>
+                            <span className="label">{tabItem.label}</span>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="content" style={{color: 'red'}}>
+                {
+                    tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tabs
