bdc591fdbb62254449180eaed4a13251a8f293c9
josh098741 <morongemokaya@gmail.com>
2025-09-27 14:55:34 +0300

    Additional commit

---
+++ custom-scroll-indicator/src/App.jsx
@@ -0,0 +1,10 @@
+import ScrollIndicator from './scroll-indicator/index'
+import './App.css'
+
+function App() {
+  return(
+    <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/>
+  );
+}
+
+export default App
